4198f7b127862facd5fc8df8d0bc76e9851e0d8a
Pankaj Adhyapak <pankaj.adhyapak@everphone.de>
2022-09-01 09:22:45 +0200

    update

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,83 +1,47 @@
-vim.g.nvim_tree_width = 25
-vim.g.nvim_tree_indent_markers = 0
-vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_highlight_opened_files = 1
 
-vim.g.nvim_tree_show_icons = {
-	git = 1,
-	folders = 1,
-	files = 1,
-	folder_arrows = 1,
-}
+-- setup with all defaults
+-- each of these are documented in `:help nvim-tree.OPTION_NAME`
+require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
 
-vim.g.nvim_tree_icons = {
-	default = "",
-	symlink = "",
-	git = {
-		unstaged = "",
-		staged = "",
-		unmerged = "",
-		renamed = "凜",
-		untracked = "",
-		deleted = "",
-		ignored = "",
-	},
-	folder = {
-		arrow_open = "",
-		arrow_closed = "",
-		default = "",
-		open = "",
-		empty = "", -- 
-		empty_open = "",
-		symlink = "",
-		symlink_open = "",
-	},
-}
-
-require("nvim-tree").setup({
-	disable_netrw = true,
-	hijack_netrw = true,
-	open_on_setup = false,
-	ignore_ft_on_setup = { "startify", "dashboard" },
-	auto_close = true,
-	open_on_tab = false,
-	hijack_cursor = true,
-	update_cwd = true,
-	update_to_buf_dir = { enable = true, auto_open = true },
-	diagnostics = {
-		enable = false,
-		icons = { hint = "", info = "", warning = "", error = "" },
-	},
-	update_focused_file = { enable = false, update_cwd = false, ignore_list = {} },
-	system_open = { cmd = nil, args = {} },
-	git = {
-		ignore = false,
-	},
-	filters = {
-		dotfiles = true,
-		custom = {
-			".git",
-			"node_modules",
-			".cache",
-		},
-	},
-	actions = {
-		open_file = {
-			quit_on_open = true,
-			resize_window = false,
-			window_picker = {
-				enable = true,
-				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-				exclude = {
-					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-					buftype = { "nofile", "terminal", "help" },
-				},
-			},
-		},
-	},
-	view = {
-		width = 25,
-		side = "left",
-		auto_resize = true,
-	},
-})
+  disable_netrw = true,
+  sort_by = "name",
+  update_cwd = false,
+  view = {
+    width = 30,
+  },
+  renderer = {
+    icons = {
+      webdev_colors = true,
+    },
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+    ignore_list = {},
+  },
+  filters = {
+    dotfiles = false,
+    custom = {},
+    exclude = {},
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 400,
+  },
+  actions = {
+    use_system_clipboard = true,
+    change_dir = {
+      enable = true,
+      global = false,
+    },
+    open_file = {
+      quit_on_open = true,
+      resize_window = false,
+    },
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+} -- END_DEFAULT_OPTS
